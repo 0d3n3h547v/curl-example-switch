0eedc5df6a224795a2482a6796059a8347fcf860
Dontwait00 <35642213+Dontwait00@users.noreply.github.com>
2018-11-23 21:13:16 +0100

    Add files via upload

---
+++ source/main.c
@@ -0,0 +1,85 @@
+/*...........Dontwait00..Project...........*/
+
+/* include */
+#include <switch.h>
+#include <string.h>
+#include <stdio.h>
+#include <curl/curl.h>
+/* define */
+#define EXIT_SUCCESS 0
+
+// this "write_callback" probably is used for writing into FILE *destinaton
+size_t write_callback(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+	return written;
+}
+
+int main(int argc, char **argv)
+{
+    gfxInitDefault();
+    consoleInit(NULL);
+	printf("press A to continue or press + to exit\n");
+	
+	CURL *curl; 
+	FILE *fp;
+	CURLcode res;
+	char *url = "http://www.projects00.000webhost.com/helloSDL2.nro"; // string used for curl_easy_setopt
+	char outfilename[FILENAME_MAX] = "sdmc:/switch/helloSDL2.nro"; // file used for curl_easy_setopt
+	curl_global_init(CURL_GLOBAL_DEFAULT); // initializing/calling functions
+	curl = curl_easy_init(); // initializing/calling functions
+	
+    // Main loop
+    while(appletMainLoop())
+    {
+        hidScanInput();
+
+        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO); // can do even: /* if( hidKeysDown(CONTROLLER_P1_AUTO) & KEY_PLUS) {//...} */ but is not very efficient.
+
+        if (kDown & KEY_PLUS) goto EXIT; // terminate the program.
+		
+		if (kDown & KEY_A) break; // break loop
+		
+		gfxFlushBuffers();
+        gfxSwapBuffers();
+	}
+	
+	consoleClear();
+	printf("starting libcurl...");
+    
+    if (curl)
+	{
+        fp = fopen(outfilename,"wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url); // getting URL from char *url
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); // skipping cert. verification, if needed (ACTUALLY IS NEEDED FROM LIBNX)
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L); // skipping hostname verification, if needed (ACTUALLY IS NEEDED FROM LIBNX)
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback); // writes data into pointer
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp); // writes pointer into FILE *destination
+        res = curl_easy_perform(curl); // perform tasks curl_easy_setopt asked before
+		printf("\n\nURL = %s\nFile = %s\n\nDownload...", url, outfilename);
+		if (res != CURLE_OK) {
+			printf("\nfailed: %s\n", curl_easy_strerror(res));
+		}
+		
+	curl_easy_cleanup(curl); // always cleanup
+    fclose(fp); // closing FILE *fp
+	
+	}
+	
+	printf("\ndone! Press B or + to exit");
+				
+	while(appletMainLoop())
+	{
+		hidScanInput();
+		u32 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+		if ((kDown & KEY_B) || (kDown & KEY_PLUS)) break; /* a simple trick to use both or more keys for the same function as of this example, breaking while loop */
+		
+		gfxFlushBuffers();
+        gfxSwapBuffers();
+	}
+	
+	EXIT: //spaghetti code :(
+    gfxExit();
+	curl_global_cleanup();
+    return EXIT_SUCCESS;
+}
+
